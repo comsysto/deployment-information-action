d0d1712bfd97d3a4886837b65a621d2845a23d44
sfleischer <s.fleischer@reply.de>
2019-11-08 12:25:16 +0100

    Added input parameter for artifact base name and version

--- create/index.js
+++ create/index.js
@@ -1,58 +1,60 @@
 const core = require( '@actions/core' );
-const exec = require( '@actions/exec' );
 const fs = require('fs');
+const process = require('process');
+
+function checkingRequiredParameters(artifactBaseName, artifactVersion) {
+    core.info(`Checking inputs...`);
+    if (artifactBaseName == null) {
+        core.setFailed('Artifact base name must be defined.')
+        process.exit(1);
+    }
+
+    if (artifactVersion == null) {
+        core.setFailed('Artifact version must be defined.')
+        process.exit(1);
+    }
+}
+
+function createFileContentJson(archiveName, artifactBaseName, artifactVersion, targetPath) {
+    let fileContent = {};
+    fileContent.artifactPath = `${archiveName}/${artifactBaseName}-${artifactVersion}.jar`;
+    fileContent.manifestPath = `${archiveName}/manifest.yaml`;
+
+    let fileContentJson = JSON.stringify(fileContent, undefined, 2);
+    core.debug(fileContentJson);
+
+    core.info('Create json file in target path...');
+    // Create directory
+    fs.mkdir(targetPath, {recursive: true}, (err) => {
+        if (err) throw err;
+    });
+
+    return fileContentJson;
+}
 
 // most @actions toolkit packages have async methods
 async function run() {
     try {
+        let artifactBaseName = core.getInput('artifact-base-name');
+        let artifactVersion = core.getInput('artifact-version');
         let archiveName = core.getInput('archive-name');
         let targetPath = core.getInput('target-path');
         let targetFilename = core.getInput('target-filename');
+        core.info(artifactBaseName)
+        core.info(artifactVersion)
+
+        checkingRequiredParameters(artifactBaseName, artifactVersion);
 
         core.info(`Creating ${targetFilename}...`);
-
-        // Retrieve artifact information via gradle
-        core.info('Retrieve artifact information from gradle...');
-        let output = '';
-        let error = '';
-        const options = {};
-        options.listeners = {
-            stdout: (data) => {
-                output += data.toString();
-            },
-            stderr: (data) => {
-                error += data.toString();
-            }
-        };
-        await exec.exec(
-            './gradlew properties -q \'|\' grep \'^archivesBaseName:\' \'|\' awk \'{print $2}\'',
-            undefined,
-            options);
-        core.debug(error)
-        let archivesBaseName = output;
-
-        output = '';
-        error = '';
-        await exec.exec(
-            './gradlew properties  -q | grep \'^version:\' | awk \'{print $2}\'',
-            undefined,
-            options);
-        core.debug(error)
-        let version = output;
+        core.info('Using following input information:');
+        core.info(`Artifact base name: ${artifactBaseName}`);
+        core.info(`Artifact version: ${artifactVersion}`);
+        core.info(`Archive name: ${archiveName}`);
+        core.info(`Target path: ${targetPath}`);
+        core.info(`Target filename: ${targetFilename}`);
 
         // Create file content
-        let fileContent = {};
-        fileContent.artifactPath = `${archiveName}/${archivesBaseName}-${version}.jar`;
-        fileContent.manifestPath = `${archiveName}/manifest.yaml`;
-
-        let fileContentJson = JSON.stringify(fileContent, undefined, 2);
-        core.debug(fileContentJson);
-
-        core.info('Create json file in target path...');
-        // Create directory
-        fs.mkdir(targetPath, { recursive: true }, (err) => {
-            if (err) throw err;
-        });
+        let fileContentJson = createFileContentJson(archiveName, artifactBaseName, artifactVersion, targetPath);
 
         // Create deployment information json file
         fs.writeFile(`${targetPath}/${targetFilename}`, fileContentJson, (err) => {
--- create/index.test.js
+++ create/index.test.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
+    process.env['INPUT_ARTIFACT-BASE-NAME'] = "application-base-name";
+    process.env['INPUT_ARTIFACT-VERSION'] = "1.0.0";
     process.env['INPUT_ARCHIVE-NAME'] = "deploymentArchive";
     process.env['INPUT_TARGET-PATH'] = "./test/build";
     process.env['INPUT_TARGET-FILENAME'] = "deploymentInfo.json";
